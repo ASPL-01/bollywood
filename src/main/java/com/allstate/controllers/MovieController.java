377016f0db64d31f17912f522c5dfd17b9ede209
Chyld Medford <chyld.medford@gmail.com>
2017-01-17 08:09:51 +0530

    fin

--- src/main/java/com/allstate/controllers/MovieController.java
+++ src/main/java/com/allstate/controllers/MovieController.java
@@ -5,6 +5,8 @@ import com.allstate.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping(value = "/movies")
 public class MovieController {
@@ -23,5 +25,25 @@ public class MovieController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Movie findById(@PathVariable int id){
         return this.service.findById(id);
+    }
+
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
+    public Iterable<Movie> findAll(){
+        return this.service.findAll();
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public Movie findByTitle(@RequestParam Map<String, String> query) {
+        return this.service.findByTitle(query.get("title"));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable int id){
+        this.service.delete(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Movie update(@PathVariable int id, @RequestBody Movie movie){
+        return this.service.update(id, movie);
     }
 }
